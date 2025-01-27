f574affbfc5f10f77985330ad480a5a4a275aa23
Yassin Nurmahomed <yassin.nurmahomed@csaude.org.mz>
2025-01-27 17:02:35 +0200

    Fix setenv and add missing environment variable

--- build/backend/setenv.sh
+++ build/backend/setenv.sh
@@ -1,3 +1,3 @@
 export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Djava.awt.headless=true -XX:+UseConcMarkSweepGC -Xms1536m -Xmx6144m -XX:PermSize=768m -XX:MaxPermSize=1024m -Duser.timezone=Africa/Maputo"
-#export JAVA_OPTS+=" -agentlib:jdwp=transport=dt_socket,server=y,address=0.0.0.0:8081,suspend=n"
+#export JAVA_OPTS="${JAVA_OPTS} -agentlib:jdwp=transport=dt_socket,server=y,address=0.0.0.0:8081,suspend=n"
 export OPENMRS_RUNTIME_PROPERTIES_FILE='/usr/local/tomcat/openmrs-runtime.properties'
